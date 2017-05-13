f9b7e35dc98dfbf96cfe9a8e01ea232885149c14
Ross Breytberg <rossbreytberg@gmail.com>
2017-05-13 14:09:16 -0700

    Created start menu and try again buttons

--- index.js
+++ index.js
@@ -14,30 +14,94 @@ window.onload = () => {
       levels[i + 1].run(scene);
     });
   }
+
+  // Function to start the game
+  const showStartMenuFunc =
+    () => showStartMenu(scene, () => levels[0].run(scene));
+
   // Show red screen on failure
   for (let i = 0; i < levels.length; i++) {
-    levels[i].addFailureHandler(() => {
-      scene.addObjectToTopLayer(new BackgroundObject('red'));
-      scene.addObjectToNewLayer(new TextObject(
-        scene.width / 2,
-        scene.height / 2,
-        'Game Over',
-        '128px Helvetica, Arial',
-        'white'
-      ));
-    });
+    levels[i].addFailureHandler(
+      () => showGameOverMenu(scene, showStartMenuFunc)
+    );
   }
   // Show green screen on game completion
-  levels[levels.length - 1].addCompletionHandler(() => {
-    scene.addObjectToTopLayer(new BackgroundObject('green'));
-    scene.addObjectToNewLayer(new TextObject(
-      scene.width / 2,
-      scene.height / 2,
-      'Winner',
-      '128px Helvetica, Arial',
-      'white'
-    ));
-  });
+  levels[levels.length - 1].addCompletionHandler(
+    () => showGameWonMenu(scene, showStartMenuFunc)
+  );
+
   // Start running the first level
-  levels[0].run(scene);
+  showStartMenuFunc();
 };
+
+function showStartMenu(scene, startButtonClickHandler) {
+  scene.addObjectToTopLayer(new BackgroundObject('black'));
+  scene.addObjectToNewLayer(new TextObject(
+    scene.width / 2,
+    scene.height / 2 - 200,
+    'SHOT',
+    '128px Helvetica, Arial',
+    'white'
+  ));
+  const startButton = new ButtonObject(
+    scene.width / 2,
+    scene.height / 2,
+    210,
+    90,
+    'Start',
+    '64px Helvetica, Arial',
+    'white',
+    'orange'
+  );
+  startButton.setClickHandler(startButtonClickHandler);
+  scene.addObjectToTopLayer(startButton);
+  scene.addObjectToNewLayer(new CursorObject());
+}
+
+function showGameOverMenu(scene, restartButtonClickHandler) {
+  scene.addObjectToTopLayer(new BackgroundObject('red'));
+  scene.addObjectToNewLayer(new TextObject(
+    scene.width / 2,
+    scene.height / 2,
+    'Game Over',
+    '128px Helvetica, Arial',
+    'white'
+  ));
+  const restartButton = new ButtonObject(
+    scene.width / 2,
+    scene.height / 2 + 200,
+    350,
+    90,
+    'Try again?',
+    '64px Helvetica, Arial',
+    'white',
+    'orange'
+  );
+  restartButton.setClickHandler(restartButtonClickHandler);
+  scene.addObjectToTopLayer(restartButton);
+  scene.addObjectToNewLayer(new CursorObject());
+}
+
+function showGameWonMenu(scene, restartButtonClickHandler) {
+  scene.addObjectToTopLayer(new BackgroundObject('green'));
+  scene.addObjectToNewLayer(new TextObject(
+    scene.width / 2,
+    scene.height / 2,
+    'Winner',
+    '128px Helvetica, Arial',
+    'white'
+  ));
+  const restartButton = new ButtonObject(
+    scene.width / 2,
+    scene.height / 2 + 200,
+    350,
+    90,
+    'Try again?',
+    '64px Helvetica, Arial',
+    'white',
+    'orange'
+  );
+  restartButton.setClickHandler(restartButtonClickHandler);
+  scene.addObjectToTopLayer(restartButton);
+  scene.addObjectToNewLayer(new CursorObject());
+}
